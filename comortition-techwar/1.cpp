56e65d8e6a52747efa53f1db6227857bc7f7eee2
nishtha-thakkar <nishthathakkar3@gmail.com>
2026-01-30 10:55:40 +0530

    compitition

--- comortition-techwar/1.cpp
+++ comortition-techwar/1.cpp
@@ -1,40 +1,87 @@
 # include <iostream>
 using namespace std;
 
-int main(){
-    for(int i =1 ; i<=9 ;i++){
-        for(int j = 1 ;j<=i ; j++){
-            // printf("%d " , j);
-            if(j==1 || j==3 || j==5|| j==7||j==9){
-                printf(" * ");
-            }
-            else if(j==2){
-                printf(" 1 ");
-                }
-                else if (j==4){
-                    printf(" 3 ");
-                }
-                else if (j==6){
-                    printf(" 5 ");
-                }
-                 else if (j==8){
-                    printf(" 7 ");
-                }
+// int main(){
+//     for(int i =1 ; i<=9 ;i++){
+//         for(int j = 1 ;j<=i ; j++){
+//             // printf("%d " , j);
+//             if(j==1 || j==3 || j==5|| j==7||j==9){
+//                 printf(" * ");
+//             }
+//             else if(j==2){
+//                 printf(" 1 ");
+//                 }
+//                 else if (j==4){
+//                     printf(" 3 ");
+//                 }
+//                 else if (j==6){
+//                     printf(" 5 ");
+//                 }
+//                  else if (j==8){
+//                     printf(" 7 ");
+//                 }
 
 
             
-        }
-        printf("\n");
-    }
-    printf(" T  E  C  H  W A R  2 0 2 6 \n");
+//         }
+//         printf("\n");
+//     }
+//     printf(" T  E  C  H  W A R  2 0 2 6 \n");
 
-    for(int i = 11 ; i>=3 ; i--){
-        for(int j = 3 ; j<=i; j++){
-            printf(" %d" , j);
-        }
-        printf("\n");
-    }
-    // printf("\n");
+//     for(int i = 11 ; i>=3 ; i--){
+//         for(int j = 3 ; j<=i; j++){
+//             printf(" %d" , j);
+//         }
+//         printf("\n");
+//     }
+//     // printf("\n");
 
     
-}
+// }
+
+int main(){
+    // if(){
+    // for(int i =1 ; i<=9 ;i++){
+    //     for(int j = 1 ;j<=i ; j++){
+    //         // printf("%d " , j);
+    //         if(j==1 || j==3 || j==5|| j==7||j==9){
+    //             printf(" * ");
+    //         }
+    //         else if(j==2){
+    //             printf(" 1 ");
+    //             }
+    //             else if (j==4){
+    //                 printf(" 3 ");
+    //             }
+    //             else if (j==6){
+    //                 printf(" 5 ");
+    //             }
+    //              else if (j==8){
+    //                 printf(" 7 ");
+    //             }
+    //             printf("\n");
+
+    //         }
+    //           printf(" T  E  C  H  W A R  2 0 2 6 \n");
+
+    //         }else{
+    //             for(int i = 11 ; i>=3 ; i--){
+    //     for(int j = 3 ; j<=i; j++){
+    //         printf(" %d" , j);
+    //     }
+    //     printf("\n");
+    //         }
+
+           
+             
+    // }
+
+
+
+    //     }
+
+    }
+
+
+
+
